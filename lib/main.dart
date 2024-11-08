0d9f6c9d149cec7443afaefa43c8a192d0b6899a
xiaoyuanzi22333 <xiaoyuanzi22333@gmail.com>
2024-11-08 17:18:31 +0800

    add UI but cannot find BLE device

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,108 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
-void main() {
+class BLE_Scanner {
+  //创建一个controller控制流
+  final _bleScanController = StreamController<ScanResult?>();
+  
+  Future<void> startBleScan() async {
+    //把扫描结果添加到流里面
+    FlutterBluePlus.scanResults.listen((event) {
+      for (ScanResult element in event) {
+        _bleScanController.add(element);
+      }
+    });
+    await FlutterBluePlus.startScan(timeout: const Duration(seconds: 10));
+  }
+   //停止扫描接口
+  Future<void> stopBleScan() async {
+    await FlutterBluePlus.stopScan();
+  }
+
+  // 获取蓝牙扫描结果的 Stream,作为接口返回出去
+  Stream<ScanResult?> get bleScanStream => _bleScanController.stream;
+}
+
+
+void main(){
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Blue Plus Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const bleScanTest(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class bleScanTest extends StatefulWidget {
+  const bleScanTest({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<bleScanTest> createState() => _bleScanTestState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _bleScanTestState extends State<bleScanTest> {
+  final bleScanner = BLE_Scanner();
+  List<ScanResult>? _scanResults;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      appBar: AppBar(title: const Text('蓝牙扫描流接收测试')),
+      body: StreamBuilder<ScanResult?>(
+        stream: bleScanner.bleScanStream,
+        builder: (BuildContext context, AsyncSnapshot<ScanResult?> snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              return const Text('没有数据流');
+            case ConnectionState.active:
+              if (snapshot.hasError) {
+                return Text('Active: 错误: ${snapshot.error}');
+              } else {
+                _scanResults ??= [];
+                 if (snapshot.data!.device.platformName.isNotEmpty) {
+                _scanResults!.add(snapshot.data!);
+                 }
+                 print(_scanResults);
+                return ListView.builder(
+                  itemCount: _scanResults?.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    final scanResult = _scanResults![index];
+                    return ListTile(
+                      title: Text('设备名称: ${scanResult.device.platformName}'),
+                      subtitle: Text('设备ID: ${scanResult.device.remoteId}'),
+                      onTap: (){
+                        //连接逻辑,自己写
+                      },
+                    );
+                  },
+                );
+              }
+            case ConnectionState.waiting:
+              return const Text('等待数据流');
+            case ConnectionState.done:
+              return const Text('数据流已经关闭');
+          }
+        },
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: FloatingActionButton(onPressed: () async {
+        if (_scanResults != null) {
+          _scanResults!.clear();
+        }
+        bleScanner.startBleScan();
+      },child: const Icon(Icons.bluetooth_searching)),
     );
   }
 }
---
+++ lib/ohjeiu.dart
@@ -0,0 +1,169 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+
+void main() {
+  runApp(const MyApp());
+  // FlutterBluePlus.startScan(timeout: Duration(seconds: 4)).catchError((error) {
+  //   debugPrint("Error starting scan: $error");
+  // });
+  // debugPrint("scan end");
+
+  // // Listen to the scanning results
+
+  // FlutterBluePlus.scanResults.listen((results) {
+  //   for (var result in results) {
+  //     debugPrint('Found Bluetooth device! Name: ${result.device.platformName}, RSSI: ${result.rssi}');
+  //   }
+  // });
+  // debugPrint("ggg");
+
+  // // Optional: Stop scanning as needed
+  // Future.delayed(Duration(seconds: 4));
+  // FlutterBluePlus.stopScan();
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Blue Plus Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const BluetoothScreen(),
+    );
+  }
+}
+
+class BluetoothScreen extends StatefulWidget {
+  const BluetoothScreen({super.key});
+
+  @override
+  State<BluetoothScreen> createState() => _BluetoothScreenState();
+}
+
+class _BluetoothScreenState extends State<BluetoothScreen> {
+
+  // FlutterBluePlus flutterBlue = FlutterBluePlus();
+  List<BluetoothDevice> _connectedDevices = [];
+  List<ScanResult> _scanResults = [];
+  bool _scanning = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _startScan();
+    _getConnectedDevices();
+  }
+
+  // 扫描蓝牙设备
+  void _startScan() {
+    // debugPrint("开始扫描");debugPrint("开始扫描");debugPrint("开始扫描");debugPrint("开始扫描");
+    setState(() {
+      _scanning = true;
+    });
+
+    FlutterBluePlus.startScan(timeout: const Duration(seconds: 5)).then((_) {
+      setState(() {
+        _scanning = false;
+      });
+    });
+
+    FlutterBluePlus.scanResults.listen((results) {
+      setState(() {
+        _scanResults = results;
+      });
+    });
+    // debugPrint("扫描结束");debugPrint("扫描结束");debugPrint("扫描结束");debugPrint("扫描结束");
+  }
+
+  // 获取已连接的设备
+  void _getConnectedDevices() async {
+    List<BluetoothDevice> connectedDevices = FlutterBluePlus.connectedDevices;
+    setState(() {
+      _connectedDevices = connectedDevices;
+    });
+  }
+
+  // 连接设备
+  void _connectToDevice(BluetoothDevice device) async {
+    await device.connect(autoConnect: false).catchError((e) {
+      debugPrint("连接失败: $e");
+    });
+    _getConnectedDevices();
+  }
+
+  // 断开设备
+  void _disconnectFromDevice(BluetoothDevice device) async {
+    await device.disconnect();
+    _getConnectedDevices();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('蓝牙设备'),
+      ),
+      body: Column(
+        children: [
+          _scanning
+              ? const CircularProgressIndicator()
+              : ElevatedButton(
+                  onPressed: _startScan,
+                  child: const Text('重新扫描'),
+                ),
+          const SizedBox(height: 200),
+          const Text('已连接的设备:', style: TextStyle(fontWeight: FontWeight.bold)),
+          _buildConnectedDevicesList(),
+          const Divider(),
+          const Text('扫描到的设备:', style: TextStyle(fontWeight: FontWeight.bold)),
+          _buildScanResultsList(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildConnectedDevicesList() {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: _connectedDevices.length,
+      itemBuilder: (context, index) {
+        BluetoothDevice device = _connectedDevices[index];
+        return ListTile(
+          title: Text(device.platformName),
+          subtitle: Text(device.remoteId.toString()),
+          trailing: ElevatedButton(
+            onPressed: () => _disconnectFromDevice(device),
+            child: const Text("断开连接"),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildScanResultsList() {
+    debugPrint("扫描");debugPrint("扫描");debugPrint("扫描");debugPrint("扫描");
+    debugPrint(_scanResults.length.toString());
+    debugPrint("扫描");debugPrint("扫描");debugPrint("扫描");debugPrint("扫描");
+
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: _scanResults.length,
+      itemBuilder: (context, index) {
+        ScanResult result = _scanResults[index];
+        return ListTile(
+          title: Text(result.device.platformName.isEmpty ? '未知设备' : result.device.platformName),
+          subtitle: Text(result.device.remoteId.toString()),
+          trailing: ElevatedButton(
+            onPressed: () => _connectToDevice(result.device),
+            child: const Text('连接'),
+          ),
+        );
+      },
+    );
+  }
+}
