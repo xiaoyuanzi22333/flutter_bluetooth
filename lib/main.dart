0868751581758123d3f42251a8bda112982ea751
xiaoyuanzi22333 <xiaoyuanzi22333@gmail.com>
2024-11-12 21:01:37 +0800

    use bluetooth_classsic instead

--- lib/main.dart
+++ lib/main.dart
@@ -1,177 +1,168 @@
-import 'dart:async';
+import 'package:bluetooth_classic/models/device.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_blue_plus/flutter_blue_plus.dart';
-import 'package:permission_handler/permission_handler.dart';
+import 'dart:async';
 
+import 'package:flutter/services.dart';
+import 'package:bluetooth_classic/bluetooth_classic.dart';
+// import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 
 void main() {
   runApp(const MyApp());
-  // FlutterBluePlus.startScan(timeout: Duration(seconds: 4)).catchError((error) {
-  //   debugPrint("Error starting scan: $error");
-  // });
-  // debugPrint("scan end");
-
-  // // Listen to the scanning results
-
-  // FlutterBluePlus.scanResults.listen((results) {
-  //   for (var result in results) {
-  //     debugPrint('Found Bluetooth device! Name: ${result.device.platformName}, RSSI: ${result.rssi}');
-  //   }
-  // });
-  // debugPrint("ggg");
-
-  // // Optional: Stop scanning as needed
-  // Future.delayed(Duration(seconds: 4));
-  // FlutterBluePlus.stopScan();
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _platformVersion = 'Unknown';
+  final _bluetoothClassicPlugin = BluetoothClassic();
+  List<Device> _devices = [];
+  List<Device> _discoveredDevices = [];
+  bool _scanning = false;
+  int _deviceStatus = Device.disconnected;
+  Uint8List _data = Uint8List(0);
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+    _bluetoothClassicPlugin.onDeviceStatusChanged().listen((event) {
+      setState(() {
+        _deviceStatus = event;
+      });
+    });
+    _bluetoothClassicPlugin.onDeviceDataReceived().listen((event) {
+      setState(() {
+        _data = Uint8List.fromList([..._data, ...event]);
+      });
+    });
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    String platformVersion;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    // We also handle the message potentially returning null.
+    try {
+      platformVersion = await _bluetoothClassicPlugin.getPlatformVersion() ??
+          'Unknown platform version';
+    } on PlatformException {
+      platformVersion = 'Failed to get platform version.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _platformVersion = platformVersion;
+    });
+  }
+
+  Future<void> _getDevices() async {
+    var res = await _bluetoothClassicPlugin.getPairedDevices();
+    setState(() {
+      _devices = res;
+    });
+  }
+
+  Future<void> _scan() async {
+    if (_scanning) {
+      debugPrint("start stopping");
+      await _bluetoothClassicPlugin.stopScan();
+      debugPrint("finish stopping");
+      setState(() {
+        _scanning = false;
+      });
+    } else {
+      debugPrint("start scanning");
+      await _bluetoothClassicPlugin.startScan();
+      debugPrint("stop scanning");
+      _bluetoothClassicPlugin.onDeviceDiscovered().listen(
+        (event) {
+          debugPrint(event.toString());
+          setState(() {
+            _discoveredDevices = [..._discoveredDevices, event];
+          });
+        },
+      );
+      debugPrint(_discoveredDevices.length.toString());
+      setState(() {
+        _scanning = true;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Blue Plus Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const BluetoothScreen(),
-    );
-  }
-}
-
-class BluetoothScreen extends StatefulWidget {
-  const BluetoothScreen({super.key});
-
-  @override
-  State<BluetoothScreen> createState() => _BluetoothScreenState();
-}
-
-class _BluetoothScreenState extends State<BluetoothScreen> {
-
-  // FlutterBluePlus flutterBlue = FlutterBluePlus();
-  List<BluetoothDevice> _connectedDevices = [];
-  List<ScanResult> _scanResults = [];
-  bool _scanning = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _startScan();
-    _getConnectedDevices();
-  }
-
-  // 扫描蓝牙设备
-  void _startScan() {
-    
-    // debugPrint("开始扫描");debugPrint("开始扫描");debugPrint("开始扫描");debugPrint("开始扫描");
-    setState(() {
-      _scanning = true;
-    });
-
-    FlutterBluePlus.startScan(timeout: const Duration(seconds: 5)).then((_) {
-      setState(() {
-        _scanning = false;
-      });
-    });
-
-    FlutterBluePlus.scanResults.listen((results) {
-      setState(() {
-        _scanResults = results;
-      });
-    });
-    // debugPrint("扫描结束");debugPrint("扫描结束");debugPrint("扫描结束");debugPrint("扫描结束");
-  }
-
-  // 获取已连接的设备
-  void _getConnectedDevices() async {
-    List<BluetoothDevice> connectedDevices = FlutterBluePlus.connectedDevices;
-    setState(() {
-      _connectedDevices = connectedDevices;
-    });
-  }
-
-  // 连接设备
-  void _connectToDevice(BluetoothDevice device) async {
-    await device.connect(autoConnect: false).catchError((e) {
-      debugPrint("连接失败: $e");
-    });
-    _getConnectedDevices();
-  }
-
-  // 断开设备
-  void _disconnectFromDevice(BluetoothDevice device) async {
-    await device.disconnect();
-    _getConnectedDevices();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('蓝牙设备'),
-      ),
-      body: Column(
-        children: [
-          _scanning
-              ? const CircularProgressIndicator()
-              : ElevatedButton(
-                  onPressed: _startScan,
-                  child: const Text('重新扫描'),
-                ),
-          const SizedBox(height: 200),
-          const Text('已连接的设备:', style: TextStyle(fontWeight: FontWeight.bold)),
-          _buildConnectedDevicesList(),
-          const Divider(),
-          const Text('扫描到的设备:', style: TextStyle(fontWeight: FontWeight.bold)),
-          _buildScanResultsList(),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildConnectedDevicesList() {
-    return ListView.builder(
-      shrinkWrap: true,
-      itemCount: _connectedDevices.length,
-      itemBuilder: (context, index) {
-        BluetoothDevice device = _connectedDevices[index];
-        return ListTile(
-          title: Text(device.platformName),
-          subtitle: Text(device.remoteId.toString()),
-          trailing: ElevatedButton(
-            onPressed: () => _disconnectFromDevice(device),
-            child: const Text("断开连接"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Classic Blue Test'),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Text("Device status is $_deviceStatus"),
+              TextButton(
+                onPressed: () async {
+                  await _bluetoothClassicPlugin.initPermissions();
+                },
+                child: const Text("Check Permissions"),
+              ),
+              TextButton(
+                onPressed: _getDevices,
+                child: const Text("Get Paired Devices"),
+              ),
+              TextButton(
+                onPressed: _deviceStatus == Device.connected
+                    ? () async {
+                        await _bluetoothClassicPlugin.disconnect();
+                      }
+                    : null,
+                child: const Text("disconnect"),
+              ),
+              TextButton(
+                onPressed: _deviceStatus == Device.connected
+                    ? () async {
+                        await _bluetoothClassicPlugin.write("ping");
+                      }
+                    : null,
+                child: const Text("send ping"),
+              ),
+              Center(
+                child: Text('Running on: $_platformVersion\n'),
+              ),
+              ...[
+                for (var device in _devices)
+                  TextButton(
+                      onPressed: () async {
+                        await _bluetoothClassicPlugin.connect(device.address,
+                            "00001101-0000-1000-8000-00805f9b34fb");
+                        setState(() {
+                          _discoveredDevices = [];
+                          _devices = [];
+                        });
+                      },
+                      child: Text(device.name ?? device.address))
+              ],
+              TextButton(
+                onPressed: _scan,
+                child: Text(_scanning ? "Stop Scan" : "Start Scan"),
+              ),
+              ...[
+                for (var device in _discoveredDevices)
+                  Text(device.name ?? device.address)
+              ],
+              Text("Received data: ${String.fromCharCodes(_data)}"),
+            ],
           ),
-        );
-      },
-    );
-  }
-
-  Widget _buildScanResultsList() {
-    debugPrint("扫描");debugPrint("扫描");debugPrint("扫描");debugPrint("扫描");
-    debugPrint(_scanResults.length.toString());
-    var status = Permission.bluetooth.status;
-    // debugPrint(status.toString());
-    status.then((val) {
-      debugPrint(val.toString());
-    });
-    debugPrint("扫描");debugPrint("扫描");debugPrint("扫描");debugPrint("扫描");
-
-    return ListView.builder(
-      shrinkWrap: true,
-      itemCount: _scanResults.length,
-      itemBuilder: (context, index) {
-        ScanResult result = _scanResults[index];
-        return ListTile(
-          title: Text(result.device.platformName.isEmpty ? '未知设备' : result.device.platformName),
-          subtitle: Text(result.device.remoteId.toString()),
-          trailing: ElevatedButton(
-            onPressed: () => _connectToDevice(result.device),
-            child: const Text('连接'),
-          ),
-        );
-      },
+        ),
+      ),
     );
   }
 }
